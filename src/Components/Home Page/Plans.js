edd50588dc6cf94b84b06d57f2fb11e4d6730d63
Danish <khandanish36408@gmail.com>
2024-04-05 23:56:24 +0530

    :bricks:setup

---
+++ src/Components/Home Page/Plans.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import '../Styles/plan.css';
+import Tick from '../Images/check-mark.png'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+function Plans() {
+    const [arr, arrset] = useState([]);
+
+    useEffect(() => {
+        
+            let f1= async ()=>{
+                const data = await axios.get("/plans/top3");
+                console.log(data,123443);
+                arrset(data.data.data);
+        }
+        let res=f1();
+        res.catch(console.error)
+    
+            // catch (err) {
+            //     console.log(err);
+            // }
+        
+        
+
+        
+    }, [])
+
+    return (
+        <div className='plansCard'>
+            <div className='h1Box'>
+                <h1 className='h1'>START EATING HEALTHY TODAY</h1>
+                <div className="line"></div>
+            </div>
+            <div className='planDetails'>
+                {arr && arr?.map((ele, key) =>
+                    <div className='pCard' key={key}>
+                        <h3 className='h3' >{ele.name}</h3>
+                        <div className='pCard1'>
+                            <div className='priceBox'>
+                                <div className='price'>Rs {ele.price}</div>
+                                <div className="duration">/ month</div>
+                            </div>
+                            <p className="point">That’s only `${ele.price/ele.duration}`₹ per meal</p>
+                        </div>
+
+                        <div className='pCard2'>
+                            <div className='ppoints'>
+                                <img src={Tick} alt='' className='img' />
+                                <p className='point'>{ele.duration} meal every month</p>
+                            </div>
+                            <div className='ppoints'>
+                                <img src={Tick} alt='' className='img' />
+                                <p className='point'>{ele.discount} discount available.</p>
+                            </div>
+                            <div className='ppoints'>
+                                <img src={Tick} alt='' className='img' />
+                                <p className='point'>{ele.ratingsAverage} rated meal.</p>
+                            </div>
+                        </div>
+                        <button className='btn'>I'm Hungry</button>
+                    </div>
+                )}
+            </div>
+        
+        </div>
+    )
+}
+
+export default Plans
